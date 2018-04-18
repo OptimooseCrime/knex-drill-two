7f93045c2d3caaf3271c8875e869d56cc6f3502f
Adam Basila <adamcabasila@gmail.com>
2018-04-18 14:24:22 -0600

    initialized repo

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres:///memory2'
+  },
+
+
+
+
+
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+
+
+  }
+
+};
